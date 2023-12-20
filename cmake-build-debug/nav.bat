12c0e6d3996522a4d27c0a0268487a78174bbb68
Jackie <202632559@qq.com>
2023-12-20 23:36:32 +0800

    delete redundant files

--- cmake-build-debug/nav.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if "%1" == "h" goto begin
-mshta vbscript:createobject("wscript.shell").run("""%~nx0""h",0)(window.close)&&exit
-:begin
-cd ./graphviz_lite2
-set startDir=%cd%
-npm run serve
